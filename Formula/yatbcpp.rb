82221195106a4e860de274e41234a060addc21a9
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-25 21:16:25 +0000

    [CI] tanks travis, todo add ci check for headergards or so

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "46f18aaa7139913fa9ecefc9e34fc131391ae5b62070f8ddbeab9b679ef8cfd3"
+    sha256 "00c6a64725e9474647ffc39651a33d793407df4fed9bfd7c8d32fa0b1d6ad395"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
