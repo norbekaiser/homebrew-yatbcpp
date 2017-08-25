069797cfd9cc6edd115c4dccfdc0745b7362d5bb
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-25 18:00:27 +0000

    [CI] reworked deployment to mirros , github, gitlab etc, added two new types and modifeid user once more

---
+++ Formula/yatbcpp.rb
@@ -0,0 +1,15 @@
+class Yatbcpp < Formula
+    homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
+    url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
+    sha256 "46f18aaa7139913fa9ecefc9e34fc131391ae5b62070f8ddbeab9b679ef8cfd3"
+
+    depends_on "cmake" => :build
+    depends_on "jsoncpp"
+    depends_on "curl"
+
+    def install
+        system "cmake", ".", *std_cmake_args
+        system "make", "install"
+    end
+
+end
