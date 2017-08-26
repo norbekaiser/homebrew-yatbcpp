1ddddf4e29fc8a9d364973e4ce8b32643796f592
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-26 13:04:56 +0000

    [CI] Modified See Also, maybe do a ci job for it?

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "4b6ee28478061f9928cbd38e9a1ee79f50ec39dfe6fc0de90a522bf33c037078"
+    sha256 "fb14987a3b2b14c145ecbdc12e9a0ac25dc8d0c638348ff5c665b0222287e3d3"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
