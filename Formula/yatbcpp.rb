882c723ea6d139772328205d2f8ff8da3af2e4fc
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-26 13:20:18 +0000

    [CI] ci step which checks for See also bla api link type

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "fb14987a3b2b14c145ecbdc12e9a0ac25dc8d0c638348ff5c665b0222287e3d3"
+    sha256 "b5ed3c79e5506f821484ea768046c5bb6b673bd9ba27c6c139713ccf1770771a"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
