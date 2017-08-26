7b27ba9e2ead761eeff5a6eb89ebcf33a41b8955
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-26 11:21:49 +0000

    [CI] Merge remote-tracking branch 'origin/master'

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "00c6a64725e9474647ffc39651a33d793407df4fed9bfd7c8d32fa0b1d6ad395"
+    sha256 "a566aec1fb8ef8f91c7ead17c9d965fc56430fa57a9df89ebbc19ed32b3f4dee"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
