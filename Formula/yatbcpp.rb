051fe1293e972b83a0be661554de7110bb239cfa
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-27 07:32:54 +0000

    [CI] sigh, outcommented too much, todo ci idea, check how many variables compared to official docu are existent

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "92adb40fca0b2de9e40fd46a4a9569525cfd094d55c8a73dad0c37e10ef9b670"
+    sha256 "b4020dfaaaca417c946b3ca33d582860ed061b768e2f9138aef8445772545f7f"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
