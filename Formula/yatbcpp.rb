192427e5c4c9a692c1b8c0ed1358030500af5040
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-28 21:03:38 +0000

    [CI] modified json conversions, todo check include order

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "b4020dfaaaca417c946b3ca33d582860ed061b768e2f9138aef8445772545f7f"
+    sha256 "b534b0b87277f7f8d8ad220f06a6e9ac164f244084222b708c112f19ea80d8a3"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
