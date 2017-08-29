afade0d77942746b1cec7ad3b7f7da10ff539be7
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-29 15:12:59 +0000

    [CI] modified readme, and little fix for sendAudio

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "b3186b4ea9f70a7a9c48e317952a46ad771e66f36194a7917e6a212ddbe002ca"
+    sha256 "23899aac3e70ba5b7683b606a2014b6df427b970459697cdd828edbf212c44e9"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
