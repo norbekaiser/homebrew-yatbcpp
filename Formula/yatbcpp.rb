00d98b64a0ca7f3c04ca182103695d7e37d88928
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-29 15:11:46 +0000

    [CI] sending local files is now a thing

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "7b40fd0233d698e7e08e84251fd7e27d438bac46ccb86aa75bda2e78e9eb757c"
+    sha256 "b3186b4ea9f70a7a9c48e317952a46ad771e66f36194a7917e6a212ddbe002ca"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
