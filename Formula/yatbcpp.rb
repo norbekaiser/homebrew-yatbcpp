6c7c55044cf57a1af9a146543d09e862cf3554c0
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-29 07:44:25 +0000

    [CI] added sendPhoto to includes , it workes

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "28787f932833c83f1d50b7b8d83b7f3da3cedb5feb01251b9de97936908cdfbb"
+    sha256 "f5510c127fd34b60d177b8e32f9ecc63808eb87e7f45ba645a36ebf4a8f417db"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
