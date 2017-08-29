e860d0e8fdf45def2c46113ea2a5385a257a542e
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-29 08:30:28 +0000

    [CI] adding sendAudio Functionality, todo test

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "f5510c127fd34b60d177b8e32f9ecc63808eb87e7f45ba645a36ebf4a8f417db"
+    sha256 "47ed63f79fbfc16f050be46f082d88b6e74cab6376ddf537aa2c2ab61f676e99"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
