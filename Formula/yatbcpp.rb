cdc5ca760ffbc01345101e1245c4a6661aef0697
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-29 15:04:15 +0000

    [CI] modified some things about multipart structure, preapring for deploy, tests pending

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "5cf8d1fa5ab6fb58f2f318bad819da00fb19d4f4aa7b534a8bb62ed1de18e2c4"
+    sha256 "7b40fd0233d698e7e08e84251fd7e27d438bac46ccb86aa75bda2e78e9eb757c"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
