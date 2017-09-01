bcec0537bb02b3182c8d1364a36abeb2c818e2b3
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-09-01 07:03:33 +0000

    [CI] there was a shild release label but ci did not set it up correctly :/

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "6e24e9018937e18f4dd106a91eacb77bbe82433936ad9ef4477a52571559de08"
+    sha256 "d485b85842133cd07bf80516128ae3e33d59a61afd6069cac8ea5a1a0c927867"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
