ca9c9038a628c9c028f548b2af8a6b27bf05c89d
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-09-01 23:22:28 +0000

    [CI] preparing inline query thing , however result serialization does not yet work

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "32b8bc01f1e89a65210de33a18e46832b20e93391b5549d66dedce4b2416947c"
+    sha256 "5d4ac29dfbb4f00187d5bd39525c9d58a25bf89a13b9a8a2d75b54b664af9c06"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
