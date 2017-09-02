853292e0d81b2c6460f56e57243acbba75f2cd30
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-09-02 00:05:04 +0000

    [CI] basic inline works somehow, trying out, and adding more typses sonon

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "5d4ac29dfbb4f00187d5bd39525c9d58a25bf89a13b9a8a2d75b54b664af9c06"
+    sha256 "e248607dd596d3549ed808643a4c9f6aa8322a20010b3c669e0915b6d856383e"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
