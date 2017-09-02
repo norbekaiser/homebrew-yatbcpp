c6780dec1bab7cae757b467123b20e237f31efc6
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-09-02 00:19:53 +0000

    [CI] Modified Token reading , since it breaks around 2 am in the morning with get, #shouldahvestayedwithC

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "e248607dd596d3549ed808643a4c9f6aa8322a20010b3c669e0915b6d856383e"
+    sha256 "98a046ad64c65262965d88abda6178d9ac8c06073e292b4d7e723e3eaef89382"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
