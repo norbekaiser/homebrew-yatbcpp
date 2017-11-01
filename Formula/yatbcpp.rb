89e7f42e736a2a11a21e6625404cfabdc6fe3997
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-01 17:00:28 +0000

    [CI] modified User output and added return type isBot

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "e3b218186e5b5be176758313725e8a1d5d9f8b7f9a0c80fdd3c294fa300b7cb4"
+    sha256 "255055fa1986b656fe5944ddbcfe35d4d212bfdb10413fb96c731bb430869036"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
