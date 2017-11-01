20298ee65122b0659d5e5d3dc5802f38234e2e7d
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-01 17:15:34 +0000

    [CI] Added Indent

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "255055fa1986b656fe5944ddbcfe35d4d212bfdb10413fb96c731bb430869036"
+    sha256 "5464498c7f4be45347f8daf8ce1c524ee1d44094c89bb2cafc52b9655a3e9eb5"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
