42e556e122787be26363aae147c43ce65f9efc26
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-03 16:16:10 +0000

    [CI] added travis again now with docker, lets hope it works

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "417f1d47f671a83a7ec197535ed852ec309bd968688ea35672df804d68b8fdce"
+    sha256 "70b88319dcc19c7c6d3d9a367ace563070b56bcae546be3a5c528c3c2f760c28"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
