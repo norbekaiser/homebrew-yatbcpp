87964966c18da786d8af453f3f9315670975d59c
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-03 16:29:38 +0000

    [CI] added debian build to docker builds

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "9a7ebda22b8f06f80e24f07e1bb9c31bcfaa5d3e4f40e164a72895a5f87d5651"
+    sha256 "b92baa09dd1251f8709274bf4cd4f4c699b038156067849f00f8d86d501a9a90"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
