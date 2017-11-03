9e62920d2566fe55547125c21f797c0305c4ff9e
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-03 08:48:31 +0000

    [CI] jenen trick mal ausprobieren, betrifft aber nur den macos mirror

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "a7300f1747d327c3d2bdfad7a79d52e1ff8d1ab0151103207bcbffb7fb017a91"
+    sha256 "417f1d47f671a83a7ec197535ed852ec309bd968688ea35672df804d68b8fdce"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
