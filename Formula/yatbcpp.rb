dba51ac0a46c50d6cc839fabb50d6639e1831e3e
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-03 16:34:12 +0000

    [CI] yep still more comfortable with gitlab ci, but travis ci should now also work like a charm

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "b92baa09dd1251f8709274bf4cd4f4c699b038156067849f00f8d86d501a9a90"
+    sha256 "ad9e4bde74e70d2849b23299d0e1654615ddcbe554a2d87d71b44c11b587296b"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
