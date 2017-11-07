e29a69b3eee713593acab85c8a50dedc108b15a4
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-07 11:09:43 +0000

    [CI] removedvwhich clause

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "258ddc7f4ee834e82d34cf2af42ff5e1480e1280e5e1501157797b209d646795"
+    sha256 "a1cf83d74c6f1a91d701aaeffe853b5877755ac13943b25603e14e2262e7d330"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
