259531bd4f8ee487f6f54549e54a3b9ee6a5db76
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-07 10:07:42 +0000

    [CI] Update github-mirror.travis-ci.yml, adding clang build to travis

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "5fa663bba435c976bd5b3b22cecd2ecd97cc037dae6d357b4ab0c57145231a2a"
+    sha256 "0367aa315e42663a527233f47b8801418628ebe3cf191caa2b301bd81d6de365"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
