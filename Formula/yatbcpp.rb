0c05486f40d19334fd2e01ade489333c0df651d7
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-08 18:16:39 +0000

    [CI] killing tnow tweh tagent

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "20e6af910e610002d3eab6675616764da305199f0825f02d2d3b41752245cfc4"
+    sha256 "51f675dc0bd72be04f1fa241e4e81ef7970519423381c02b5f0c9f0a754fbd6f"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
