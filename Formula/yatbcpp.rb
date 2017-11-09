d7b0d2439d0d2f28c3e01482db9c84145ac4c354
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-09 16:40:55 +0000

    [CI] bot is now able to send messages via it's own token

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "51f675dc0bd72be04f1fa241e4e81ef7970519423381c02b5f0c9f0a754fbd6f"
+    sha256 "338028059e8c4e955a6ad75c072c4fcf6528aa9c386023a1f17fbaeb53890352"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
