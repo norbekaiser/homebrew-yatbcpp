5bb05953a2e72877f4373a910f221265f6854178
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-11 12:03:58 +0000

    [CI] modified parsing, and added psoibility to parse from string

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "7c2147128317b4f695d8f422f0b668a01b2e38fce49ef62d5b7e3c290a6024b3"
+    sha256 "9bae4c2c51f6e1f59463351c53820409f50222cd2f8f7d105b579ce0b34cf482"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
