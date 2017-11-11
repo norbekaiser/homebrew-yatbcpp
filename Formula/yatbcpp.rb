1964fde2ffb205672bb6be5ea667c5b83d6ce1bc
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-11 13:25:17 +0000

    [CI] made enums-> enum classes, to prevent yatbcpp::creator instead of yatbcpp::chatmeber_status::creator

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "4d089bab99f457defe0e3c4fe3bb0066f937c0c5d548ee6c7323bca6c02ebe8d"
+    sha256 "9ee7ef6e89222729a2331db9b0448c8dd16e8b33450060662bb9b83197ba8eb6"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
