618e2fb7d87dfd24b041d676fc3f60b56b14d1cb
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-01-01 10:41:18 +0000

    [CI] deploying install md, todo separate install mds for the deployment targets

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "40270383064e487b9742de3015dd9437d62f0dfc483fc8e8195fa727f36e9acd"
+    sha256 "ffbf14853b8cf983946e01a9bb8a42b4b9eaf9babc815bf9937bcb6bb8205b91"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
