ccf43c9b892f12efa37c7bb7c184b7f5daac8523
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-01-01 16:50:24 +0000

    [CI] nutze ich den export header? hmm eig nich, hoffentlich is der nicht wichtig

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "5ffe639a2a4ad9069fc533296a76e78681ee6e2deda67e2cbfeeca821b339790"
+    sha256 "00570aa7e211321f829bc9c129fd07a116389613b6856b2a8acaa79431b6435d"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
