304de943cb0afb1dc1ef8bb38adfa9bc22b86202
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-01-01 01:19:51 +0000

    [CI] Merge branch 'pbuilder-start' into 'master'
    PBUILDER integration und div send funktionen
    
    See merge request yatbcpp/yatbcpp!1

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "9ee7ef6e89222729a2331db9b0448c8dd16e8b33450060662bb9b83197ba8eb6"
+    sha256 "40270383064e487b9742de3015dd9437d62f0dfc483fc8e8195fa727f36e9acd"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
