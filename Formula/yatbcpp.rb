9aeda3c25ca83ddbe9a41f588a1789606300db27
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-01-01 16:56:00 +0000

    [CI] error in package naming found at the generated debian files

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "00570aa7e211321f829bc9c129fd07a116389613b6856b2a8acaa79431b6435d"
+    sha256 "738d3bdda94a77a6501a23ee640fd1808bf265d0279eb3b152d038e5f94d9e76"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
