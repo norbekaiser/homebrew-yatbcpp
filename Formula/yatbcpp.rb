34aff7e358a8bcacc35fa11ae14c4a2a16f7e359
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-01-01 17:03:05 +0000

    [CI] added deployment info for gitlab

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "738d3bdda94a77a6501a23ee640fd1808bf265d0279eb3b152d038e5f94d9e76"
+    sha256 "54602626135ca0ee1bebf79063202c5f8b5730db1aceaa1da593ae02d3bd7e24"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
