343ad9a2a1f11f48a01a5cb7df51d2c9dcbee3ef
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-02-02 23:44:38 +0000

    [CI] Merge branch 'fix_naming_issue' into 'master'
    found problem with naming of library, especially in packages
    
    See merge request yatbcpp/yatbcpp!2

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "54602626135ca0ee1bebf79063202c5f8b5730db1aceaa1da593ae02d3bd7e24"
+    sha256 "b2a40d3183dfaf0ebf75a49ede920c91fb75010c7d15e4d8f91820bd732056db"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
