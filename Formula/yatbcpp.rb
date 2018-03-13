77180bac5147462d2f821861fb6edf1b850c6bf2
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-03-13 21:04:32 +0000

    [CI] homebrew formula deployment modified, todo modif furhter so that pure github is possible

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "0539acf71a178657b8a389c110ce6356ca76882dda3075c4b318c06b25a0524e"
+    sha256 "2870978dd4c4e8a5ee35848df286cced0122017415f0088985e1d148a7c31f78"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
