9399cc2d8a02ead2051f2aecb176ed0f9ab87cae
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-03-13 20:06:09 +0000

    [CI] Merge branch 'master' of gitlab.norbert-ruehl.de:yatbcpp/yatbcpp

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "123723ededefd5145e94a4e13120e7ebf8a58e68db8097d1b659c8c5dee665e6"
+    sha256 "627931942b230f4d4873f74b7d615c2ec12470d34a179ce3138a5a73cda5b561"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
