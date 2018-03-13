f717218f45dfe39b9df7b4536da4120f30d5ad33
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-03-13 20:55:43 +0000

    [CI] modified key handling in containers

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "627931942b230f4d4873f74b7d615c2ec12470d34a179ce3138a5a73cda5b561"
+    sha256 "0539acf71a178657b8a389c110ce6356ca76882dda3075c4b318c06b25a0524e"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
