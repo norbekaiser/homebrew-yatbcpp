0c5094c0a4e557062c87fa32182f3262d15f338a
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-03-13 19:51:56 +0000

    [CI] ed was missing in preapre src containertn

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "8e4f8e9b18668c33cc4b23937ce72b69e12fe73f79fd85d56ca63a9b5da51de8"
+    sha256 "123723ededefd5145e94a4e13120e7ebf8a58e68db8097d1b659c8c5dee665e6"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
