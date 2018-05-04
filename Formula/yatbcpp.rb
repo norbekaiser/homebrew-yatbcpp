a6422d9af72bc486832ad219867f929c6feeefac
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-05-04 09:54:55 +0000

    [CI] Merge branch 'ptrfun' into 'master'
    Ptrfun
    
    See merge request yatbcpp/yatbcpp!4

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "bcdb31890abbca7c7a639756e9cbb69152083b2da7f20c1674444d010db9d5a6"
+    sha256 "5a9e8e2597733b7cf72d96719b90dca98cd57cad5575b3731ff407a3ec385f13"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
