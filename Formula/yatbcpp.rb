0d1c140de6d2edc080c3128ba8c2ac7e95979035
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-07-18 17:47:15 +0000

    [CI] modified control file, in hope this might fix an issue, still maybe consider adding curl/jsoncpp as external dependency in src

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "0a9aee7144eba5a5909cb4918465e1664a447ab7ba000ad973156f2d8ac8df3d"
+    sha256 "3706ca42c896b7de0c62494bacf10833ac5ace3a102986d1179f5588abc03d59"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
