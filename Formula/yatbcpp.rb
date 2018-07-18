1ad466e29a4c7b2d0b07abe7b14177d1e50506eb
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-07-18 15:00:26 +0000

    [CI] removed libcurl as dependency, as installs fail with libcurl3 and libcurl4, one however currently should be installed, maybe in near future, curl is linked statically into lib

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "e68246a5f64890092c33c52b6716df85ca42332f7a3b150bfcfc6bcf3186e3cb"
+    sha256 "0a9aee7144eba5a5909cb4918465e1664a447ab7ba000ad973156f2d8ac8df3d"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
