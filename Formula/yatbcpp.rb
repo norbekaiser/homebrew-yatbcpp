f281c40a9ba563af54939cc005191b5fdea98c31
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2018-07-20 10:03:37 +0000

    [CI] moved dockerized homebrew deploy to fixed lxc container, redo docker in future for homebrew deployment

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "3706ca42c896b7de0c62494bacf10833ac5ace3a102986d1179f5588abc03d59"
+    sha256 "5a8dd3842ac21b2a7bd3385f7feab20364c6deb2e44077d3874d97b7bee4c9e1"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
