aafb2422924c341030c5c88bf42580c87ff2bb01
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2019-01-03 13:08:11 +0000

    [CI] modified documentation building, todo put build inside of containers

--- Formula/yatbcpp.rb
+++ Formula/yatbcpp.rb
@@ -1,7 +1,7 @@
 class Yatbcpp < Formula
     homepage "https://github.com/norbekaiser/homebrew-yatbcpp"
     url "https://yatbcpp.norbert-ruehl.de/download/homebrew-yatbcpp-0.0.1.tar.gz"
-    sha256 "cb532e724ea0e127ef939afe51389b8901d42f6a839efdccc627b76f65dd61b0"
+    sha256 "e8c6cacce82b1e58aac40465b1b82811c710ee55fb9dd89e295f8da95b4c52f0"
 
     depends_on "cmake" => :build
     depends_on "jsoncpp"
